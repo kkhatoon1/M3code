94a81a8eefb6f86f4490077c2ec6aab882f41ba0
kkhatoon1 <kkhatoon1@my.nl.edu>
2024-10-11 09:37:06 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,7 @@
+#khateeja khatoon
+#oct 10 2024 
+#this program computes the area of circle 
+radius = float(input("enter radius value"))
+area=round(3.14*radius*radius)
+print(f"thanks for using this to calculate area of a circle and here is the answer: ", area)
+
---
+++ p4.py
@@ -0,0 +1,7 @@
+#khateeja khatoon
+#oct 10 2024
+# This program computes the MPG for a car
+miles = float(input("enter number of miles car har driven: "))
+gallon = float(input("enter how many gallon of gas is used: "))
+MPG=miles/gallon
+print(f" here is the total mpg of your car : {MPG} ")
